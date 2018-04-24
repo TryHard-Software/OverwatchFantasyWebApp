64a18bc8d82eb4ac670de2169e47b6ed3eeec56c
Ian Shaffer <ianrshaffer@gmail.com>
2018-04-23 17:57:22 -0700

    mobile no longer shows landing video

--- client/js/home.js
+++ client/js/home.js
@@ -4,10 +4,12 @@ $(document).ready(function () {
     var myvid = document.getElementById('bg-video');
     
     if ($(window).width() < 798) {
-        myvid.src="/images/videos/bg-video-mobile.mp4"
+        myvid.src=null;
+        $(".landing-background").attr("src","/images/landing.png")
     }
 
     $("#bg-video").on("ended", function () {
+        console.log("wat")
         myvid.play();
     });
    
