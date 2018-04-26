ec37fb280c275f4ad26725b60db855cc94aa896d
Ian Shaffer <ianrshaffer@gmail.com>
2018-04-25 18:38:10 -0700

    wrong way

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -207,7 +207,7 @@ function getLiveFeedHistory(req, res) {
                     results[i].victim_heroid = victim_heroid;
                 }
                 results.sort(function (a, b) {
-                    return new Date(b.createdAt) - new Date(a.createdAt);
+                    return new Date(b.createdAt) + new Date(a.createdAt);
                 });
                 res.json(results);
             }))
